c9dc19d4418faedcf9f351992aa54c0fc839667d
fernandohiroshi <fernandohiroshidev@gmail.com>
2024-09-06 02:28:46 -0300

    style(SkeletonThree): adjust padding for video element

--- src/components/view/hero/features/components/skeletons/SkeletonThree.tsx
+++ src/components/view/hero/features/components/skeletons/SkeletonThree.tsx
@@ -11,7 +11,7 @@ export function SkeletonThree() {
         <div className="relative flex h-auto w-full flex-1 flex-col space-y-2">
           <video
             src="https://cdn.cosmicjs.com/7a1734b0-da64-11ee-b6bc-0f88b0862d1c-home-video.mp4"
-            className="aspect-square h-full w-full object-cover p-2"
+            className="aspect-square h-full w-full object-cover pt-2"
             autoPlay
             muted
             loop
