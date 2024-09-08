eb0eb52dc76aa8afbb055eb395e85112bef99873
fernandohiroshi <fernandohiroshidev@gmail.com>
2024-09-08 13:11:23 -0300

    fix: correct typo

--- src/components/view/hero/features/components/skeletons/SkeletonThree.tsx
+++ src/components/view/hero/features/components/skeletons/SkeletonThree.tsx
@@ -10,7 +10,7 @@ export function SkeletonThree() {
       <div className="group mx-auto h-full w-full bg-transparent dark:bg-transparent">
         <div className="relative flex h-auto w-full flex-1 flex-col space-y-2">
           <video
-            src="hhttps://cdn.cosmicjs.com/8eba5610-6dfb-11ef-a492-5bdc7520fe60-cms.mp4"
+            src="https://cdn.cosmicjs.com/8eba5610-6dfb-11ef-a492-5bdc7520fe60-cms.mp4"
             className="aspect-square h-full w-full object-cover pt-2"
             autoPlay
             muted
