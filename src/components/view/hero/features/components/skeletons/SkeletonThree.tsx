951fa3316ea1d406144b4a37a8cfc61092730732
fernandohiroshi <fernandohiroshidev@gmail.com>
2024-09-08 13:04:08 -0300

    chore: compress images and video

--- src/components/view/hero/features/components/skeletons/SkeletonThree.tsx
+++ src/components/view/hero/features/components/skeletons/SkeletonThree.tsx
@@ -10,7 +10,7 @@ export function SkeletonThree() {
       <div className="group mx-auto h-full w-full bg-transparent dark:bg-transparent">
         <div className="relative flex h-auto w-full flex-1 flex-col space-y-2">
           <video
-            src="https://cdn.cosmicjs.com/7a1734b0-da64-11ee-b6bc-0f88b0862d1c-home-video.mp4"
+            src="hhttps://cdn.cosmicjs.com/8eba5610-6dfb-11ef-a492-5bdc7520fe60-cms.mp4"
             className="aspect-square h-full w-full object-cover pt-2"
             autoPlay
             muted
