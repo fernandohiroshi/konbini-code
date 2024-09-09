8afd8b29fb9c0969865533570d3415e7861056e0
fernandohiroshi <fernandohiroshidev@gmail.com>
2024-09-08 22:21:57 -0300

    style: update styles

--- src/components/view/templates/components/AnimateTitle.tsx
+++ src/components/view/templates/components/AnimateTitle.tsx
@@ -9,7 +9,9 @@ export function AnimateTitle() {
     <div className="pb-20">
       <h1 className="relative z-20 mx-auto max-w-7xl bg-gradient-to-b from-zinc-800 via-zinc-700 to-zinc-700 bg-clip-text text-center text-xl font-semibold text-transparent dark:from-zinc-800 dark:via-white dark:to-white md:text-xl">
         <Cover>
-          <span className="text-4xl lg:text-7xl">{t('section-title')}</span>
+          <span className="text-4xl md:text-6xl lg:text-7xl">
+            {t('section-title')}
+          </span>
         </Cover>{' '}
         <br />
         {t('description')}{' '}
