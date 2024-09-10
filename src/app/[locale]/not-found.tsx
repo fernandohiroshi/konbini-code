edfd5fd1d4bf2ad840a8e9a738312de90514028f
fernandohiroshi <fernandohiroshidev@gmail.com>
2024-09-10 11:58:18 -0300

    feat: create not-found page and bg-gradient component

---
+++ src/app/[locale]/not-found.tsx
@@ -0,0 +1,25 @@
+import { House } from 'lucide-react'
+import Link from 'next/link'
+import React from 'react'
+
+import { BackgroundGradientAnimation } from '@/components/ui/aceternity/bg-gradient'
+
+export default function NotFound() {
+  return (
+    <main>
+      <BackgroundGradientAnimation>
+        <div className="pointer-events-none absolute inset-0 z-50 flex items-center justify-center px-4 text-center text-3xl font-bold text-white md:text-4xl lg:text-7xl">
+          <p className="bg-gradient-to-b from-white/80 to-white/20 bg-clip-text text-transparent drop-shadow-2xl">
+            ERROR 404
+          </p>
+        </div>
+      </BackgroundGradientAnimation>
+      <Link
+        href="/"
+        className="absolute bottom-8 right-8 rounded-full bg-black/60 p-4 text-white shadow-2xl duration-500 ease-in-out hover:scale-90 hover:bg-white/80 hover:text-black"
+      >
+        <House size={30} />
+      </Link>
+    </main>
+  )
+}
