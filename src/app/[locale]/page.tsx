d2a2db11e0c98dc4edd09a9938c98df0e7dd7e56
fernandohiroshi <fernandohiroshidev@gmail.com>
2024-09-12 10:37:12 -0300

    feat: create Copyright component

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,8 +1,10 @@
+import { Copyright } from '@/components/view/copy/Copy'
 import { Footer } from '@/components/view/footer/Footer'
 import { MenuMobile } from '@/components/view/header/components/menuMobile/MenuMobile'
 import { Header } from '@/components/view/header/Header'
 import { Features } from '@/components/view/hero/features/Features'
 import { Questions } from '@/components/view/questions/Questions'
+import { ScrollUpToggle } from '@/components/view/scrollUpToggle/ScrollUpToggle'
 import { Services } from '@/components/view/services/Services'
 import { Templates } from '@/components/view/templates/Templates'
 
@@ -16,11 +18,10 @@ export default function Home() {
         <Templates />
         <Questions />
         <Footer />
-        <p className="mb:text-sm mb-8 text-xs opacity-85 md:text-center">
-          © {`${new Date().getFullYear()}`} Konbini Code. All rights reserved.
-        </p>
+        <Copyright />
         <MenuMobile />
       </main>
+      <ScrollUpToggle />
     </>
   )
 }
---
+++ src/components/view/copy/Copy.tsx
@@ -0,0 +1,7 @@
+export function Copyright() {
+  return (
+    <p className="mb:text-sm mb-8 text-xs opacity-85 md:text-center">
+      © {`${new Date().getFullYear()}`} Konbini Code. All rights reserved.
+    </p>
+  )
+}
