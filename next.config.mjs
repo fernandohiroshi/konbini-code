96cec3758af964811fc481d96222fcc6e718891c
fernandohiroshi <fernandohiroshidev@gmail.com>
2024-09-12 13:11:30 -0300

    chore: update imgix hostname in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const withNextIntl = createNextIntlPlugin()
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [{ protocol: 'https', hostname: 'cdn.cosmicjs.com' }],
+    remotePatterns: [{ protocol: 'https', hostname: 'imgix.cosmicjs.com' }],
   },
 }
 
