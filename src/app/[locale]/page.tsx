b3e8719d1d3ad810fd139bda3edb24fb05972ffd
fernandohiroshi <fernandohiroshidev@gmail.com>
2024-09-11 21:37:34 -0300

    style: update styles in Home and copyright

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,4 +1,5 @@
 import { Footer } from '@/components/view/footer/Footer'
+import { MenuMobile } from '@/components/view/header/components/menuMobile/MenuMobile'
 import { Header } from '@/components/view/header/Header'
 import { Features } from '@/components/view/hero/features/Features'
 import { Questions } from '@/components/view/questions/Questions'
@@ -9,15 +10,16 @@ export default function Home() {
   return (
     <>
       <Header />
-      <main className="mx-auto max-w-7xl scroll-mt-24 px-3 md:px-4">
+      <main className="mx-auto max-w-7xl scroll-mt-24 px-4">
         <Features />
         <Services />
         <Templates />
         <Questions />
         <Footer />
-        <p className="mb:text-sm mb-8 text-center text-xs opacity-85">
+        <p className="mb:text-sm mb-8 text-xs opacity-85 md:text-center">
           © {`${new Date().getFullYear()}`} Konbini Code. All rights reserved.
         </p>
+        <MenuMobile />
       </main>
     </>
   )
