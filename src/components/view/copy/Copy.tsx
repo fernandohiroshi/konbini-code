baf60147aa22f688c315cd6cb37f833cbc3d181d
fernandohiroshi <contatofernandohiroshitakeda@outlook.com>
2024-12-04 07:16:45 -0300

    chore: update copyright in Copy component

--- src/components/view/copy/Copy.tsx
+++ src/components/view/copy/Copy.tsx
@@ -1,7 +1,7 @@
 export function Copyright() {
   return (
     <p className="mb:text-sm mb-8 text-xs opacity-85 md:text-center">
-      © {`${new Date().getFullYear()}`} Konbini Code. All rights reserved.
+      © {`${new Date().getFullYear()}`} Konbini Code, All rights reserved.
     </p>
   )
 }
