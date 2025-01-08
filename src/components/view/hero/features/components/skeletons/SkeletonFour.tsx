d212aabb6dae881236a450a73c9b2a315bb82877
fernandohiroshi <contatofernandohiroshitakeda@outlook.com>
2025-01-07 23:30:01 -0300

    fix(SkeletonFour): adjust for mobile horizontal view

--- src/components/view/hero/features/components/skeletons/SkeletonFour.tsx
+++ src/components/view/hero/features/components/skeletons/SkeletonFour.tsx
@@ -2,7 +2,7 @@ import { Globe } from '../globe/Globe'
 
 export function SkeletonFour() {
   return (
-    <div className="relative mt-10 hidden h-60 flex-col items-center bg-transparent dark:bg-transparent md:flex md:h-60">
+    <div className="relative mt-10 hidden h-60 flex-col items-center bg-transparent dark:bg-transparent md:h-60 lg:flex">
       <Globe className="absolute -bottom-80 -right-10 md:-bottom-72 md:-right-10" />
     </div>
   )
